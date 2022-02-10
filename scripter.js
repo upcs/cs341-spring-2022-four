97a141bc1d1245ab8ca285210d920144573adea0
Jennifer Brana <jenniferbrana1@gmail.com>
2022-02-09 18:20:05 -0800

    bottomnavbar

---
+++ scripter.js
@@ -0,0 +1,33 @@
+
+/* script for navbar */
+
+  var coll = document.getElementsByClassName("collapsible");
+  var i;
+
+  for (i = 0; i < coll.length; i++) {
+    coll[i].addEventListener("click", function() {
+      this.classList.toggle("active");
+      var content = this.nextElementSibling;
+      if (content.style.display === "block") {
+        content.style.display = "none";
+      } else {
+        content.style.display = "block";
+      }
+    });
+  }
+
+
+
+/* script for dropdown */
+/* source: https://www.w3schools.com/howto/howto_js_filter_dropdown.asp */
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+function filterFunction() {
+  var input, filter, ul, li, a, i;
+  div = document.getElementById("myDropdown");
+  a = div.getElementsByTagName("a");
+}

94cd0c96d4994cecf3cccd45c9d3e02f01a59a95
BH <74618590+harringt23@users.noreply.github.com>
2022-02-15 11:08:25 -0800

    finialized creating a synched stylesheet

--- trekup/public/javascripts/homepage_script.js
+++ trekup/public/javascripts/homepage_script.js
@@ -1,15 +1,15 @@
-$(document).ready(function(){
-  $("#mastHead").click(function(){
-    window.location.href="Index.html";
-  });
-  $("#profile").click(function(){
-    let loggedIn = true;
+$(document).ready(function() {
+    $("#mastHead").click(function() {
+        window.location.href = "Index.html";
+    });
+    $("#profile").click(function() {
+        let loggedIn = true;
 
-    if(loggedIn){
-      window.location.href="profile.html"
-    }else{
-      window.location.href="login_page.html";
-    }
-  });
+        if (loggedIn) {
+            window.location.href = "profile.html"
+        } else {
+            window.location.href = "login_page.html";
+        }
+    });
 
 });

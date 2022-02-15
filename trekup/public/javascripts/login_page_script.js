1e18afa6505398a75c7e384c54bae7f8e5f6dbe4
Francisco Nguyen <63086526+nguyen-fran@users.noreply.github.com>
2022-02-15 11:10:30 -0800

    added create an account page

--- trekup/public/javascripts/login_page_script.js
+++ trekup/public/javascripts/login_page_script.js
@@ -1,10 +1,17 @@
-// loginHandler = function(event) {
-//     location.href = "profile.html";
-// }
+loginHandler = function(event) {
+    window.location.href = "profile.html";
+    event.preventDefault();
+}
+
+createAccountHandler = function(event) {
+    window.location.href = "create_account_page.html"
+}
 
 $(document).ready(function(){
     //actions for when the submit button for the form is clicked
-    // $("#login_button").on("click", loginHandler);
+    $("#login_button").on("click", loginHandler);
+
+    $("#create_account_button").on("click", createAccountHandler);
 
   $("#mastHead").click(function(){
     window.location.href="Index.html";

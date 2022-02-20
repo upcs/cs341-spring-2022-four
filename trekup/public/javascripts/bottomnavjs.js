8fe7aea2f4522c4b7f986fd00c72559d3b17b965
Jennifer Brana <jenniferbrana1@gmail.com>
2022-02-20 14:00:37 -0800

    send post to get dummy hikes

--- trekup/public/javascripts/bottomnavjs.js
+++ trekup/public/javascripts/bottomnavjs.js
@@ -17,64 +17,104 @@
   }
 
 
-
+//function for when the user clicks "search" button to search for hikes by name
 function searchSearch(){
   var searched = document.getElementById("search").value;
 
   document.getElementById("hike1").innerHTML = "displayed hike";
   document.getElementById("hike2").innerHTML = "displayed hike";
   document.getElementById("hike3").innerHTML = "displayed hike";
-  document.getElementById("hike4").innerHTML = "displayed hike";
-  document.getElementById("hike5").innerHTML = searched;
 }
 
 
-/* script for dropdown */
+/* script for displaying dropdowns */
 /* source: https://www.w3schools.com/howto/howto_js_filter_dropdown.asp */
 /* When the user clicks on the button,
 toggle between hiding and showing the dropdown content */
 function filterDifficulty() {
   document.getElementById("difficultyDrop").classList.toggle("show");
 }
-
 function filterMiles() {
   document.getElementById("milesdropdown").classList.toggle("show");
 }
-
-
 function filterElevation() {
   document.getElementById("elevationGain").classList.toggle("show");
 }
 
 
 
-
-
-
-
-
 /* function to display hikes when user hits submit */
 function searchHikes(){
-  document.getElementById("hike1").innerHTML = "displayed hike";
-  document.getElementById("hike2").innerHTML = "displayed hike";
-  document.getElementById("hike3").innerHTML = "displayed hike";
-  document.getElementById("hike4").innerHTML = "displayed hike";
-  document.getElementById("hike5").innerHTML = "displayed hike";
-
-
-
   //get checked boxes for difficulty
   var diffEasy = document.getElementById("easy").checked;
   var diffMod = document.getElementById("moderate").checked;
   var diffHard = document.getElementById("hard").checked;
 
-  //get miles range
+  //get mile range
   var miles = document.getElementById("mileRange").value;
 
   //get elevation range
   var elevation = document.getElementById("elevationRange").value;
 
+  var difficultyS = "null";
+
+
+  if(diffEasy && diffMod && diffHard){
+    difficultyS = "easy-hard";
+  }
+  else if (diffEasy && diffMod) {
+    difficultyS = "easy-moderate";
+  }
+  else if (diffEasy && diffHard) {
+    difficultyS = "easy&hard";
+  }
+  else if (diffHard && diffMod) {
+    difficultyS = "moderate-hard";
+  }
+  else if (diffEasy) {
+    difficultyS = "easy";
+  }
+  else if (diffMod) {
+    difficultyS = "moderate";
+  }
+  else if (diffHard) {
+    difficultyS = "hard";
+  }
+
+
+
+  //display hikes
+  document.getElementById("selection").innerHTML = "Filters selected: Miles = " + miles + " miles, " + "Elevation = " + elevation + " ft, " + "Difficulty = " + difficultyS;
+  // document.getElementById("milesSelected").innerHTML = "Miles: " + miles + " miles";
+  // document.getElementById("elevationSelected").innerHTML = "Elevation: " + elevation + " ft";
+  // document.getElementById("difficultySelected").innerHTML = "Difficulty: ";
+  // document.getElementById("hike4").innerHTML = "displayed hike";
+  // document.getElementById("hike5").innerHTML = "displayed hike";
+
+  sendPostFilters();
+
+
 }
+
+
+//function to send a post to the database
+//  ***currently gets dummy data
+function sendPostFilters(){
+  $.post('/filterPost', //url
+    function(dummyHikes, status, json){ //callback function
+
+        $("#hike1").text(dummyHikes.data[0].hike1.distance + " miles, " + dummyHikes.data[0].hike1.elevation + " ft, " + dummyHikes.data[0].hike1.difficulty + ".");
+        $("#hike2").text(dummyHikes.data[1].hike2.distance + " miles, " + dummyHikes.data[1].hike2.elevation + " ft, " + dummyHikes.data[1].hike2.difficulty + ".");
+        $("#hike3").text(dummyHikes.data[2].hike3.distance + " miles, " + dummyHikes.data[2].hike3.elevation + " ft, " + dummyHikes.data[2].hike3.difficulty + ".");
+
+    });
+}
+
+
+
+
+
+
 
 
 

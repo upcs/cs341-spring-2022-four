05e653bff85ededa7433e52d25735e5d21384eab
BH <74618590+harringt23@users.noreply.github.com>
2022-02-20 13:43:40 -0800

    created initial files for the posts

---
+++ trekup/routes/new_user.js
@@ -0,0 +1,10 @@
+/* new_user.js
+ * Homework 5 
+ * @author Brynn Harrington
+ * Date Modified: 10 Feb 2022
+ * 
+ * Modify the way that the router 
+ * posts so the order is processed correctly
+ * 
+ * Used: https://www.w3schools.com/js/js_json_intro.asp
+ */

d83234d17bce66ac7d978b11e589b00c9a7401b3
BH <74618590+harringt23@users.noreply.github.com>
2022-02-22 10:14:50 -0800

    supertest installed

--- trekup/tests/login.test.js
+++ trekup/tests/login.test.js
@@ -1,9 +1,17 @@
 /* login.test
  * Brynn Harrington 
- * Date Modified: 10 Feb 2022
+ * Date Modified: 22 Feb 2022
  *
  * This unit test verifies the correct 
  * functionality of the users route 
  * 
  * STATUS: ?
+ * 
+ * Used: https://dev.to/franciscomendes10866/testing-express-api-with-jest-and-supertest-3gf
+ * Used: https://www.npmjs.com/package/supertest 
  */
+
+// use super test to bound the port without needing the exact port number
+import supertest from 'supertest'
+import app from './app.js'
+// test if the post sends to users

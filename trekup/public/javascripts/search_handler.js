864fc33c71009fa9e9f3260944eb1e1d8471447d
BH <74618590+harringt23@users.noreply.github.com>
2022-02-23 16:25:56 -0800

    changed diff. to radio buttons so no mult. selection

--- trekup/public/javascripts/search_handler.js
+++ trekup/public/javascripts/search_handler.js
@@ -2,6 +2,40 @@
  * Brynn Harrington 
  * Date Modified: 23 Feb 2022
  *
- * Handles when the search button is pressed
+ * Handles when the search button and
+ * its respective filters when it is pressed
  * 
  */
+
+/* validate_trail_name
+ * checks if the search name was empty 
+ */
+validate_trail_name = function() {
+    // get the name of the hike being searched for
+    var trail_name = document.getElementById("trail_name").ariaValueMax;
+
+    // verify they searched something 
+    if (trail_name == "") {
+        alert("Please enter a trail name or select a filter.")
+    }
+}
+
+search_filter_handler = function(e) {
+    // prevent default behavior of refreshing website
+    e.preventDefault();
+
+    /* get the attributes of the hike being searched for */
+    // trail name
+    var trail_name = $("trail_name").val();
+    // difficulty 
+    // var diff = $("#difficultyDrop").children(".current")[0];
+    var diff = $("input[name='");
+
+    // verify they searched something 
+    if (trail_name == "diff") {
+        alert("Please enter a trail name or select a filter.")
+    }
+
+    // extract each value of the filter if any
+
+}

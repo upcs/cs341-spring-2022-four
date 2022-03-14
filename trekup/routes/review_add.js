dc47c1cea6ea73ec3a8d77ad3507c27413ba9403
BH <74618590+harringt23@users.noreply.github.com>
2022-03-13 19:16:44 -0700

    change password functionality

--- trekup/routes/review_add.js
+++ trekup/routes/review_add.js
@@ -19,7 +19,18 @@ var trail_info = [];
 // POST for adding a review to a hike
 router.post('/', function(req, res, next) {
     // values necessary for the review
+    var hike = req.query.hike;
     var username = req.query.name;
+    var rating = req.query.rating;
+    var comment = req.query.comment;
 
+    // add the review to the hike
+    db.dbquery(`INSERT INTO REVIEWS (HIKE, USERNAME, RATING, COMMENT) VALUES (${hike},
+        ${username}, ${rating}, ${comment})`, function(err, result) {
+        if (err) {
+            console.log(err);
+            return;
+        }
+    });
 });
 module.exports = router;
---
+++ trekup/routes/user_passChange.js
@@ -0,0 +1,58 @@
+/* user_passChange.js
+ *
+ * @author Brynn Harrington
+ * Date Modified: 13 Mar 2022
+ *  
+ * Gets a user to the USERS database
+ * as requested by the user and allows hashed 
+ * password to be change
+ * 
+ */
+// require the express and get the router
+var express = require('express');
+var router = express.Router();
+
+// require bcrypt for password encryption 
+var bcrypt = require('bcryptjs');
+
+// require the database management system
+var dbms = require('./dbms_user_info.js');
+
+// POST request to register an account
+router.post('/', function(req, res, next) {
+    // necessary values
+    var username = req.query.username;
+    var old_pass = req.query.old_pass;
+    var new_pass = req.query.new_pass;
+
+    // testing 
+    console.log(old_pass);
+
+    // get the user
+    dbms.dbquery("SELECT * FROM USERS WHERE USERNAME = '" + username + "'", function(err, result) {
+        if (err) {
+            console.log(err);
+            return;
+        }
+
+        // verify entered correct password to change
+        const found = bcrypt.compareSync(old_pass, result[0].PASSWORD);
+        if (found) {
+            const hashed_pass = bcrypt.hashSync(new_pass, 10);
+            dbms.dbquery("UPDATE USERS SET PASSWORD = '" + hashed_pass + "' WHERE USERNAME = '" + username + "'",
+                function(error, result) {
+                    if (err) {
+                        console.log(err);
+                        return;
+                    }
+                }
+            );
+            // store the result 
+            res.json(result);
+        } else {
+            res.json(null);
+        }
+    });
+});
+
+module.exports = router;

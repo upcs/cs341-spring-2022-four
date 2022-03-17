53f34d4c13fb9de6eec89e34435fb021f228e01b
Gareth <oogarethrice@gmail.com>
2022-03-17 09:36:07 -0700

    Filter and search

--- trekup/public/posting/popSearchHikes.js
+++ trekup/public/posting/popSearchHikes.js
@@ -4,8 +4,18 @@ var router = express.Router();
 var dbq = require('../../routes/dbms_promise');
 
 router.post('/', async(req, res, next)=>{
-  var hi1 = JSON.stringify(await dbq.dbquery("SELECT * FROM TRAIL_INFO LIMIT 10"));
-  res.send(hi1); 
+
+  if(req.body.dif){
+      var hi1 = JSON.stringify(await dbq.dbquery("SELECT * FROM TRAIL_INFO WHERE DIFFICULTY = " + "'" + req.body.dif + "'" + " LIMIT 8"));
+  }else if(req.body.nam){
+    var hi1 = JSON.stringify(await dbq.dbquery("SELECT * FROM TRAIL_INFO WHERE HIKE = " + "'" + req.body.nam + "'"));
+  }else{
+      var hi1 = JSON.stringify(await dbq.dbquery("SELECT * FROM TRAIL_INFO LIMIT 8"));
+  }
+
+
+
+  res.send(hi1);
 });
 
 module.exports = router;

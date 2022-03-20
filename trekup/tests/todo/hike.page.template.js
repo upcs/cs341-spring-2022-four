9c65b59ea2ef615f1de817249285e1a950f52ad0
BH <74618590+harringt23@users.noreply.github.com>
2022-03-20 13:49:56 -0700

    created the files and added supertest

---
+++ trekup/tests/todo/hike.page.template.js
@@ -0,0 +1,10 @@
+/* hike.page.template
+ * Brynn Harrington 
+ * Date Modified: 15 Mar 2022
+ *
+ * This unit test verifies a user's account 
+ * is successfully created in the server
+ * 
+ * STATUS: ?
+ *
+ */

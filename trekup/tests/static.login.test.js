cb2dc2183062f9d3d13002b6183fb79b19a2abc0
BH <74618590+harringt23@users.noreply.github.com>
2022-03-24 02:05:27 -0700

    working on static test for user DB

---
+++ trekup/tests/static.login.test.js
@@ -0,0 +1,23 @@
+/* static.login.test
+ * Brynn Harrington 
+ * Date Modified: 23 Mar 2022
+ *
+ * Tests the operations associated with
+ * user functionality 
+ * STATUS: ?
+ * 
+ */
+/* GLOBAL VARIABLES */
+// require file system, query, and javascript
+var fs = require('fs');
+// var $ = require('jquery');
+var js = require('../public/javascripts/profile_page_script');
+
+// load in the window with HTML elements
+// window.$ = $;
+
+/* USERNAME TEST */
+test('get username', () => {
+    document.body.innerHTML = 'div'
+});
+/* PASSWORD TEST */

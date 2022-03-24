1cb0f7c559acaa55b0233b2f3ed3abf77fa0360f
Jennifer Brana <jenniferbrana1@gmail.com>
2022-03-24 10:08:50 -0700

    posting for filtering move

--- trekup/public/javascripts/bottomnavjs.js
+++ trekup/public/javascripts/bottomnavjs.js
@@ -1,6 +1,81 @@
 
 /* script for navbar */
 
+function searchHikesFilter(){
+  //get checked boxes for difficulty
+  // var diffEasy = document.getElementById("easy").checked;
+  // var diffMod = document.getElementById("moderate").checked;
+  // var diffHard = document.getElementById("hard").checked;
+  var diff = "Easy";
+  var diffEasy = "0";
+  var diffMod = "0";
+  var diffHard = "0";
+  if ($('#easy').is(":checked")){
+    diffEasy = "1";
+    diff = "Easy"
+  }
+  if ($('#moderate').is(":checked")){
+    diffMod = "1";
+    diff = "Moderate"
+  }
+  if ($('#hard').is(":checked")){
+  // if(document.getElementById("hard").checked){
+    diffHard = "1";
+    diff = "Difficult"
+  }
+  //get mile range
+  var miles = document.getElementById("mileRange").value;
+
+  //get elevation range
+  var elevation = document.getElementById("elevationRange").value;
+
+
+  // localStorage.setItem('Diffic', diff);
+  // localStorage.setItem('Milea', miles);
+  // localStorage.setItem('Eleva', elevation);
+  // localStorage.setItem('UseFilter', true);
+
+  // $.post("/popSearchHikes",{ez: diffEasy, me: diffMod, ha: diffHard, mileR: miles, elev: elevation, noFilter: 0},function(data, status){
+  $.post("/popSearchHikes", {dif:diff, mileR: miles, elev: elevation},function(data, status){
+    displayHikePostInfo(data, status, 8);
+  });
+
+  localStorage.setItem('UseFilter', false);
+
+}
+
+function searchHikesName(){
+  var name = document.getElementById("search").value;
+
+  if(name == ""){
+    alert("No text entered in searchbar. Please enter a hike name to search");
+  }else{
+    $.post("/popSearchHikes", {nam: name}, function(data, status){
+      displayHikePostInfo(data, status, 8);
+    });
+  }
+}
+
+
+function displayHikePostInfo(data, status, numPop){
+  const recHikes = JSON.parse(data);
+  // alert(recHikes);
+  for(let i = 0; i < numPop; i++){
+    let helperStr = "";
+    if(numPop == 4){
+      helperStr = "Hike";
+    }else{
+      helperStr = "SearchHike";
+    }
+    populateRecHikes(recHikes, i, helperStr);
+  }
+  // populateRecHikes(recHikes, 0, "Hike");
+  // populateRecHikes(recHikes, 1, "Hike");
+  // populateRecHikes(recHikes, 2, "Hike");
+  // populateRecHikes(recHikes, 3, "Hike");
+}
+
+
   var coll = document.getElementsByClassName("collapsible");
   var i;
 
@@ -131,18 +206,18 @@ function filterElevation() {
 
 //function to send a post to the database
 //  ***currently gets dummy data
-function sendPostFilters(miles, elevation, difficultyS){
-  $.post('/filterPost', //url
-    function(dummyHikes, status, json){ //callback function
-
-      document.getElementById("selection").innerHTML = "Filters selected: Miles = " + miles + " miles, " + "Elevation = " + elevation + " ft, " + "Difficulty = " + difficultyS;
-
-      $("#hike1").text(dummyHikes.data[0].hike1.name + ": " + dummyHikes.data[0].hike1.distance + " miles, " + dummyHikes.data[0].hike1.elevation + " ft, " + dummyHikes.data[0].hike1.difficulty + ".");
-      $("#hike2").text(dummyHikes.data[1].hike2.name + ": " + dummyHikes.data[1].hike2.distance + " miles, " + dummyHikes.data[1].hike2.elevation + " ft, " + dummyHikes.data[1].hike2.difficulty + ".");
-      $("#hike3").text(dummyHikes.data[2].hike3.name + ": " + dummyHikes.data[2].hike3.distance + " miles, " + dummyHikes.data[2].hike3.elevation + " ft, " + dummyHikes.data[2].hike3.difficulty + ".");
-
-    });
-}
+// function sendPostFilters(miles, elevation, difficultyS){
+//   $.post('/filterPost', //url
+//     function(dummyHikes, status, json){ //callback function
+//
+//       document.getElementById("selection").innerHTML = "Filters selected: Miles = " + miles + " miles, " + "Elevation = " + elevation + " ft, " + "Difficulty = " + difficultyS;
+//
+//       $("#hike1").text(dummyHikes.data[0].hike1.name + ": " + dummyHikes.data[0].hike1.distance + " miles, " + dummyHikes.data[0].hike1.elevation + " ft, " + dummyHikes.data[0].hike1.difficulty + ".");
+//       $("#hike2").text(dummyHikes.data[1].hike2.name + ": " + dummyHikes.data[1].hike2.distance + " miles, " + dummyHikes.data[1].hike2.elevation + " ft, " + dummyHikes.data[1].hike2.difficulty + ".");
+//       $("#hike3").text(dummyHikes.data[2].hike3.name + ": " + dummyHikes.data[2].hike3.distance + " miles, " + dummyHikes.data[2].hike3.elevation + " ft, " + dummyHikes.data[2].hike3.difficulty + ".");
+//
+//     });
+// }
 
 // For ranges
 var mileRange = document.querySelector('input[name="mileRange"]'),

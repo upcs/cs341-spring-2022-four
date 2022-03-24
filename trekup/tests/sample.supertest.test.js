2267bfdbedfd3e1f7c6277d9836f86a3f1e65b5b
BH <74618590+harringt23@users.noreply.github.com>
2022-03-24 10:28:31 -0700

    working on account testing

--- trekup/tests/sample.supertest.test.js
+++ trekup/tests/sample.supertest.test.js
@@ -11,17 +11,18 @@ const request = require('supertest');
 const assert = require('assert');
 const express = require('express');
 
-const app = express();
+// const app = express();
 
-app.get('/user', function(req, res) {
-    res.status(200).json({ name: 'john' });
-});
+// app.get('/user', function(req, res) {
+//     res.status(200).json({ name: 'john' });
+// });
 
-request(app)
-    .get('/user')
-    .expect('Content-Type', /json/)
-    .expect('Content-Length', '15')
-    .expect(200)
-    .end(function(err, res) {
-        if (err) throw err;
-    });
+// request(app)
+//     .get('/user')
+//     .expect('Content-Type', /json/)
+//     .expect('Content-Length', '15')
+//     .expect(200)
+//     .end(function(err, res) {
+//         if (err) throw err;
+//     });
+//medium.com/@sampath.katari/using-supertest-for-testing-node-js-apis-fb076b20ee41
--- trekup/tests/static.login.test.js
+++ trekup/tests/static.login.test.js
@@ -7,17 +7,55 @@
  * STATUS: ?
  * 
  */
+
 /* GLOBAL VARIABLES */
 // require file system, query, and javascript
-var fs = require('fs');
+var $ = require('jquery');
 // var $ = require('jquery');
 var js = require('../public/javascripts/profile_page_script');
+/* BUTTON FUNCTIONALITY TESTS */
+test('login button test', () => {
+    // find the username in the HTML document
+    document.body.innerHTML = '<div> \
+    <input type="text" id="email"> </div>';
 
-// load in the window with HTML elements
-// window.$ = $;
+    // get the value of the username
+    document.getElementById('email').value = 'f.nguyen123'
+
+    // verify correct username
+    // expect(js.)
+    // wait until the function is fully set up
+});
+
 
 /* USERNAME TEST */
-test('get username', () => {
-    document.body.innerHTML = 'div'
+test('get user', () => {
+    // find the username in the HTML document
+    document.body.innerHTML = '<div> \
+    <input type="text" id="email"> </div>';
+
+    // get the value of the username
+    document.getElementById('email').value = 'f.nguyen123'
+
+    // verify correct username
+    // expect(js.)
+    // wait until the function is fully set up
 });
+
 /* PASSWORD TEST */
+
+// var $ = require('jquery');
+// var f = require('../public/javascripts/login');
+// test('getting username input', () => {
+// document.body.innerHTML = '<div> \
+//     <input type="text" id="loginUsername"> </div>';
+//     document.getElementById('loginUsername').value = 'doej21';
+//     expect(f.getUsername()).toBe('doej21');
+// });
+
+// test('getting username input', () => {
+//     document.body.innerHTML = '<div> \
+//     <input type="text" id="loginPassword"> </div>';
+//     document.getElementById('loginPassword').value = 'password';
+//     expect(f.getPassword()).toBe('password');
+// });

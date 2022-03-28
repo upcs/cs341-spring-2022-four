061e6e2743dc0450c59b05c83cafcee6d99e2fe3
BH <74618590+harringt23@users.noreply.github.com>
2022-03-28 16:01:34 -0700

    get working - post NOT

--- trekup/tests/server.get.test.js
+++ trekup/tests/server.get.test.js
@@ -1,21 +1,19 @@
-/* account.test
+/* server.get.test
  * Brynn Harrington 
  * Date Modified: 23 Mar 2022
  *
  * Tests the operations associated with
- * user functionality 
+ * GET functionality 
  * 
  * STATUS: PASSED (yay)
  * 
  * SOURCE: https: //www.npmjs.com/package/start-server-and-test-with-options  
  */
-const supertest = require('supertest');
 var request = require('supertest'); // use the super test for server testing
 var app = require('../app'); // get the app
-var server = supertest.agent("http://localhost:3000"); // server running
 
 // timeout to prevent too long for a test running
-jest.setTimeout(5000);
+// jest.setTimeout(5000);
 
 // testing whether the response of getting valid 
 describe('Test the root path', () => {
@@ -23,6 +21,18 @@ describe('Test the root path', () => {
     it('GET method response - 200 expected for success', done => {
         // test the get method responds with success response 
         request(app).get('/').then(response => {
+            expect(response.statusCode).toBe(200);
+            done();
+        });
+    });
+});
+
+// testing whether the response of getting valid 
+describe('Test the profile posting path', () => {
+    // note: "it" just fancy for "test"
+    it('POST method response - 200 expected for success', done => {
+        // test the get method responds with success response 
+        request(app).post('/user_add').then(response => {
             expect(response.statusCode).toBe(200);
             done();
         });

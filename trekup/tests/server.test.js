ab67059b4ce45d83ec257e7857f4e55118fe71ab
BH <74618590+harringt23@users.noreply.github.com>
2022-03-28 15:36:25 -0700

    server test passed

--- trekup/tests/server.test.js
+++ trekup/tests/server.test.js
@@ -1,17 +1,28 @@
-/* To test the server, the
- * start-server-and-test npm script 
- * is used in the package.json file.
+/* account.test
+ * Brynn Harrington 
+ * Date Modified: 23 Mar 2022
+ *
+ * Tests the operations associated with
+ * user functionality 
+ * 
+ * STATUS: PASSED (yay)
+ * 
+ * SOURCE: https: //www.npmjs.com/package/start-server-and-test-with-options  
+ */
+const supertest = require('supertest');
+var request = require('supertest'); // use the super test for server testing
+var app = require('../app'); // get the app
+var server = supertest.agent("http://localhost:3000"); // server running
 
-SOURCE: https://www.npmjs.com/package/start-server-and-test-with-options  
-*/
-const request = require('supertest');
-const app = require('../app');
+// timeout to prevent too long for a test running
+jest.setTimeout(5000);
 
-// jest.setTimeout(100000);
-
+// testing whether the response of getting valid 
 describe('Test the root path', () => {
-    test('It should response the GET method', done => {
-        return request(app).get('/').then(response => {
+    // note: "it" just fancy for "test"
+    it('GET method response - 200 expected for success', done => {
+        // test the get method responds with success response 
+        request(app).get('/').then(response => {
             expect(response.statusCode).toBe(200);
             done();
         });

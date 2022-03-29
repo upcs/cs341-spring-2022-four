4d0e4a4900a91905f598e1c5e74df0d691eb3e73
BH <74618590+harringt23@users.noreply.github.com>
2022-03-29 10:39:02 -0700

    deployment in russian?

--- trekup/public/javascripts/profile_page_script.js
+++ trekup/public/javascripts/profile_page_script.js
@@ -3,7 +3,7 @@
  * updated by: Francisco Nguyen
  */
 
-addStars = function (stars_num) {
+addStars = function(stars_num) {
     var star_elements = "";
     for (let i = 0; i < 5; i++) {
         star_elements += (i < stars_num) ? "<td><div class=\"rating user star\"></div></td>" : "<td><div class=\"rating user star blank\"></div></td>";
@@ -11,7 +11,7 @@ addStars = function (stars_num) {
     return star_elements;
 }
 
-addHike = function (hike) {
+addHike = function(hike) {
     return `
     <div id="hikebox", onClick='mrClicky("#ProfileHike5Name")'>
       <img id="hikepic" src="https://static.bhphotovideo.com/explora/sites/default/files/styles/top_shot/public/New-Hiking.jpg?itok=p0tfoXXi">
@@ -26,7 +26,7 @@ addHike = function (hike) {
   `;
 }
 
-makeTrophy = function (trophyName, desc) {
+makeTrophy = function(trophyName, desc) {
     return `
     <div id = trophybox>
         <h3 style="margin-left: 10px; margin-right:10px"><i>${trophyName}</i> </h3>
@@ -35,59 +35,59 @@ makeTrophy = function (trophyName, desc) {
     </div>`;
 }
 
-addTrophies = function (achievements_bits) {
-    if (achievements_bits.charAt(0) == '1') 
+addTrophies = function(achievements_bits) {
+    if (achievements_bits.charAt(0) == '1')
         $("#achievementsbox").append(makeTrophy("Pat on the Back Badge", "Congrats! You left your couch you potato."));
 
-    if (achievements_bits.charAt(1) == '1') 
+    if (achievements_bits.charAt(1) == '1')
         $("#achievementsbox").append(makeTrophy("Lurker Badge", "Hand around at the end of 5 hikes."));
 
-    if (achievements_bits.charAt(2) == '1') 
+    if (achievements_bits.charAt(2) == '1')
         $("#achievementsbox").append(makeTrophy("Impossible Badge", "It's literally impossible to get this. Why are you seeing this?."));
-    
-    if (achievements_bits.charAt(3) == '1') 
+
+    if (achievements_bits.charAt(3) == '1')
         $("#achievementsbox").append(makeTrophy("Hermit Badge", "Survive in the woods for 7 weeks."));
 
-    if (achievements_bits.charAt(4) == '1') 
+    if (achievements_bits.charAt(4) == '1')
         $("#achievementsbox").append(makeTrophy("Sprinter Badge", "Complete 60 miles in 24 hours."));
 }
 
-$(document).ready(function() { 
+$(document).ready(function() {
     $("#logoutbutton").click(function() {
         sessionStorage.clear();
-        window.location.href="index.html";
-   });
-
-   $("#updatebutton").click(function(){
-        window.location.href="update_profile.html";
+        window.location.href = "index.html";
     });
-    
-    // var usrnm = sessionStorage.getItem('current_user');
-    var usrnm = 'f.nguyen123';
+
+    $("#updatebutton").click(function() {
+        window.location.href = "update_profile.html";
+    });
+
+    var usrnm = sessionStorage.getItem('current_user');
+    //var usrnm = 'f.nguyen123';
 
     //making POST to get profile data
-    $.post('/profile_load', {username: usrnm})
-    .done(function(profile_data) {
-        $("#username").text(`@${usrnm}`);
-        $("#name").text(profile_data.NAME);
-        $("#trails_completed").text(profile_data.TRAILS_COMPLETED);
-        $("#distance_walked").text(profile_data.DISTANCE_WALKED);
-        $("#elevation_gain").text(profile_data.ELEVATION_GAINED);
+    $.post('/profile_load', { username: usrnm })
+        .done(function(profile_data) {
+            $("#username").text(`@${usrnm}`);
+            $("#name").text(profile_data.NAME);
+            $("#trails_completed").text(profile_data.TRAILS_COMPLETED);
+            $("#distance_walked").text(profile_data.DISTANCE_WALKED);
+            $("#elevation_gain").text(profile_data.ELEVATION_GAINED);
 
-        addTrophies(profile_data.ACHIEVEMENTS);
-    });
+            addTrophies(profile_data.ACHIEVEMENTS);
+        });
 
     //making POST to get profile's list of completed hikes
-    $.post('/profile_hike_list', {username: usrnm})
+    $.post('/profile_hike_list', { username: usrnm })
         .done(function(data) {
             for (let hike of data) {
                 $("#completedbox").append(addHike(hike));
             }
         });
-  });
+});
 
-function mrClicky(hikeNameField){
-  passName = $(hikeNameField).text();
-  localStorage.setItem('Name', passName);
-  window.location.href="hike_page_template.html";
+function mrClicky(hikeNameField) {
+    passName = $(hikeNameField).text();
+    localStorage.setItem('Name', passName);
+    window.location.href = "hike_page_template.html";
 }

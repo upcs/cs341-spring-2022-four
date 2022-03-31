6f1653deffe13f65969e67bf35cfa344c6af9f27
BH <74618590+harringt23@users.noreply.github.com>
2022-03-30 19:56:09 -0700

    username / password passed

--- trekup/public/javascripts/login_page_script.js
+++ trekup/public/javascripts/login_page_script.js
@@ -1,36 +1,115 @@
+/* login function
+ * 
+ * @author Francisco and Brynn 
+ * attempts to login to an account  
+ */
+function login(event) {
+    $("#login_button").on("click", loginHandler); // handle click on login
+    $("#create_account_link").on("click", goToCreateAccountHandler); // handle click on create account
+}
+
+/* loginHandler
+ * 
+ * @author Francisco and Brynn 
+ * attempts to login once click received
+ */
 loginHandler = function(event) {
-    $.post('/user_login', {
-        username: getUsername(),
-        password: getPassword()
-    }, function(data) {
-        if (data.localeCompare("user exists") == 0) {
-            sessionStorage.setItem('current_user', getUsername());
-            window.location.href = "profile.html";
-            event.preventDefault();
-        } 
-        else {
-            window.location.href = "login_page.html";
-            event.preventDefault();
+    $.post(`/login?username=${getUsername()}&password=${getPassword()}`, function(data) {
+        // verify the user exists
+        if (data != null && data.localeCompare("user exists") == 0) {
+            sessionStorage.setItem('current_user', getUsername()); // store current user
+            window.location.href = "profile.html"; // open profile page
+            event.preventDefault(); // prevent default 
+        } else {
+            window.location.href = "login_page.html"; // go to login page
+            document.getElementById('password').value = ''; // set password to empty 
+            event.preventDefault(); // prevent default 
         }
     });
     event.preventDefault();
 }
 
+/* goToCreateAccountHandler
+ * 
+ * @author Francisco and Brynn 
+ * attempts to login once click received
+ */
 goToCreateAccountHandler = function(event) {
     window.location.href = "create_account_page.html";
 }
 
-$(document).ready(function() {
-    //actions for when the submit button for the form is clicked
-    $("#login_button").on("click", loginHandler);
-
-    $("#create_account_link").on("click", goToCreateAccountHandler);
-});
-
+/* getUsername
+ * 
+ * @author Francisco and Brynn 
+ * gets the username  
+ */
 function getUsername() {
-    return $("#username").val();
+    var username = document.getElementById('username').value;
+    // username =
+    return username;
 }
 
+/* getPassword
+ * 
+ * @author Francisco and Brynn 
+ * gets the password  
+ */
 function getPassword() {
-    return $("#password").val();
+    var password = document.getElementById('password').value;
+    return password;
 }
+
+// export the functions for testing 
+module.exports = { login, getUsername, getPassword };
+// PAST CODE FOR REFERENCE
+///////////////////////////////////////////////////////////////////////////////////////////////////////
+// $.post('/user_login', {
+//     username: $("#username").val(),
+//     password: $("#password").val()
+// }, function(data) {
+//     if (data.localeCompare("user exists") == 0) {
+//         sessionStorage.setItem('current_user', $("#username").val());
+//         window.location.href = "profile.html";
+//         event.preventDefault();
+//     } else {
+//         window.location.href = "login_page.html";
+//         event.preventDefault();
+//     }
+// });
+// $(document).ready(function() {
+//     //actions for when the submit button for the form is clicked
+//     $("#login_button").on("click", loginHandler);
+
+//     $("#create_account_link").on("click", goToCreateAccountHandler);
+// });
+// loginHandler = function(event) {
+//     $.post('/user_login', {
+//         username: getUsername(),
+//         password: getPassword()
+//     }, function(data) {
+//         if (data.localeCompare("user exists") == 0) {
+//             sessionStorage.setItem('current_user', getUsername());
+//             window.location.href = "profile.html";
+//             event.preventDefault();
+//         } else {
+//             window.location.href = "login_page.html";
+//             event.preventDefault();
+//         }
+//     });
+//     event.preventDefault();
+// }
+// goToCreateAccountHandler = function(event) {
+//     window.location.href = "create_account_page.html";
+// }
+// $(document).ready(function() {
+//     // function login() {
+//     //actions for when the submit button for the form is clicked
+//     $("#login_button").on("click", loginHandler);
+//     $("#create_account_link").on("click", goToCreateAccountHandler);
+// });
+// login function
+// function login() {
+//     var username = getUsername();
+//     var password = getPassword();
+//     $("#login_button").on("click", loginHandler);
+// }
---
+++ trekup/tests/login.test.js
@@ -0,0 +1,29 @@
+/* server.get.test
+ * Brynn Harrington 
+ * Date Modified: 23 Mar 2022
+ *
+ * Tests the operations associated with
+ * GET functionality 
+ * 
+ * STATUS: PASSED (yay)
+ * 
+ * SOURCE: https: //www.npmjs.com/package/start-server-and-test-with-options  
+ */
+var $ = require('jquery');
+var f = require('../public/javascripts/login_page_script');
+
+// test getting username
+it('test username', () => {
+    document.body.innerHTML = '<div> \
+    <input type="text" id="username"> </div>';
+    document.getElementById('username').value = 'f.nguyen123';
+    expect(f.getUsername()).toBe('f.nguyen123');
+});
+
+// test getting password
+it('test password', () => {
+    document.body.innerHTML = '<div> \
+    <input type="text" id="password"> </div>';
+    document.getElementById('password').value = 'b';
+    expect(f.getPassword()).toBe('b');
+});

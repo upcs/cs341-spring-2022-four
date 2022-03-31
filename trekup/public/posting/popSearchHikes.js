0bb456848a6a6c85c4ed8c1439c516307582c5cf
Jennifer Brana <jenniferbrana1@gmail.com>
2022-03-31 10:15:48 -0700

    all filters in 1

--- trekup/public/posting/popSearchHikes.js
+++ trekup/public/posting/popSearchHikes.js
@@ -16,10 +16,10 @@ router.post('/', async(req, res, next)=>{
   if(req.body.nam){
     var hi1 = JSON.stringify(await dbq.dbquery("SELECT * FROM TRAIL_INFO WHERE HIKE = " + "'" + req.body.nam + "'"));
   }
-  else if(req.body.dif){
-    console.log(req.body.dif);
-      var hi1 = JSON.stringify(await dbq.dbquery("SELECT * FROM TRAIL_INFO WHERE DIFFICULTY = " + "'" + req.body.dif + "'" + " LIMIT 30"));
-  }
+  // else if(req.body.dif){
+  //   console.log(req.body.dif);
+  //     var hi1 = JSON.stringify(await dbq.dbquery("SELECT * FROM TRAIL_INFO WHERE DIFFICULTY = " + "'" + req.body.dif + "'" + " LIMIT 30"));
+  // }
   // else if(req.body.nam){
   //   var hi1 = JSON.stringify(await dbq.dbquery("SELECT * FROM TRAIL_INFO WHERE HIKE = " + "'" + req.body.nam + "'"));
   // }
@@ -28,13 +28,20 @@ router.post('/', async(req, res, next)=>{
   //   console.log(req.body.mileL + " miles lower");
   //   var hi1 = JSON.stringify(await dbq.dbquery("SELECT * FROM TRAIL_INFO WHERE DISTANCE BETWEEN " + "'"  + req.body.mileL + "'" + " AND "+ "'" + req.body.mileR + "'" + " LIMIT 8"));
   // }
-  else if(req.body.mileR, req.body.mileL, req.body.elevU, req.body.elevL){
+  else if(req.body.mileR, req.body.mileL, req.body.elevU, req.body.elevL, req.body.dif){
     console.log(req.body.mileR + " miles");
     console.log(req.body.mileL + " miles lower");
     console.log("upper: " + req.body.elevU + " ft");
     console.log("lower: " + req.body.elevL + " ft");
-    var hi1 = JSON.stringify(await dbq.dbquery("SELECT * FROM TRAIL_INFO WHERE DISTANCE BETWEEN " + "'"  + req.body.mileL + "'" + " AND "+ "'" + req.body.mileR + "'" + " AND ELEVATION_CHANGE BETWEEN " + "'" + req.body.elevL + "'" + " AND " + "'" + req.body.elevU + "'" + " LIMIT 8"));
+    var hi1 = JSON.stringify(await dbq.dbquery("SELECT * FROM TRAIL_INFO WHERE DISTANCE BETWEEN " + "'"  + req.body.mileL + "'" + " AND "+ "'" + req.body.mileR + "'" + " AND ELEVATION_CHANGE BETWEEN " + "'" + req.body.elevL + "'" + " AND " + "'" + req.body.elevU + "'" + " AND DIFFICULTY = " + "'" + req.body.dif + "'" + " LIMIT 8"));
   }
+  // else if(req.body.mileR, req.body.mileL, req.body.elevU, req.body.elevL){
+  //   console.log(req.body.mileR + " miles");
+  //   console.log(req.body.mileL + " miles lower");
+  //   console.log("upper: " + req.body.elevU + " ft");
+  //   console.log("lower: " + req.body.elevL + " ft");
+  //   var hi1 = JSON.stringify(await dbq.dbquery("SELECT * FROM TRAIL_INFO WHERE DISTANCE BETWEEN " + "'"  + req.body.mileL + "'" + " AND "+ "'" + req.body.mileR + "'" + " AND ELEVATION_CHANGE BETWEEN " + "'" + req.body.elevL + "'" + " AND " + "'" + req.body.elevU + "'" + " LIMIT 8"));
+  // }
   // else if(req.body.mileR, req.body.elevU){
   //   console.log(req.body.mileR + " miles");
   //   console.log(req.body.mileL + " miles lower");

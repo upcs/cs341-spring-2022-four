42802ee19d3727caccbab1b86c632b77e4aa32bc
machida23 <machida23@up.edu>
2022-04-24 16:17:26 -0700

    user trophies

--- trekup/routes/profile_load.js
+++ trekup/routes/profile_load.js
@@ -2,11 +2,13 @@
 
 var express = require('express');
 var router = express.Router();
+var user_trophies = require("./user_trophies");
 
 var dbms = require('./user_info_dbms');
 
 /* POST profile's info. */
 router.post('/', function(req, res, next) {
+
     var query = `select NAME, TRAILS_COMPLETED, DISTANCE_WALKED, ELEVATION_GAINED, ACHIEVEMENTS 
                 from USER_PROFILES where USERNAME='${req.body.username}'`;
 
@@ -18,6 +20,7 @@ router.post('/', function(req, res, next) {
         // console.log(JSON.stringify(results));
         res.json(results[0]);
     });
+    user_trophies.updateTrophies(req.body.username);
 });
 
 module.exports = router;
